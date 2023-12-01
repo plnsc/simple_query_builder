488811fae2166133eba0003488d1dc94db6bc452
Paulo Nascimento <paulornasc@gmail.com>
2023-12-01 12:37:44 -0300

    add entity, columns and rows to the query class

--- simple_query_builder/query.py
+++ simple_query_builder/query.py
@@ -1,11 +1,41 @@
 """
 
 TODO: list of what needs to be done in **query.py**
-(#): called it Query instead of Statement for the class name to be short
+(#): called it Query instead of Statement to make the class name short
+(#): the validation to make sure that the rows and columns to match eachother
+     need some thought about it
 """
 
 from simple_query_builder.dialect import _Dialect
 
 
 class Query(_Dialect):
-    pass
+    _entity: str
+    _columns: list[str]
+    _rows: list[list[str]]
+    _sql: list[str]
+
+    def __init__(self, entity: str = ''):
+        super().__init__()
+
+        self._entity = entity
+        self._columns = []
+        self._rows = []
+
+    def _get_entity(self) -> str:
+        return self.wrapper('identifier', self._entity)
+
+    def set_columns(self, columns: list[str]) -> 'Query':
+        self._columns = [self.wrapper('identifier', column)
+                         for column in columns]
+        return self
+
+    def set_row(self, row: list[str]) -> 'Query':
+        self._rows.append([self.sanitize(row) for row in row])
+        return self
+
+    def set(self, data: dict[str, any]) -> 'Query':
+        self.set_columns(data.keys())
+        self.set_row(data.values())
+
+        return self
--- tests/simple_query_builder/test_query.py
+++ tests/simple_query_builder/test_query.py
@@ -10,3 +10,87 @@ class TestQuery(unittest.TestCase):
     def test_query_alias(self):
         expected_type = q()
         assert type(expected_type) == type(self.query)
+
+    def test_set_entity(self):
+        expected = 'some_sql_table'
+
+        query = Query(expected)
+
+        assert query._entity == expected
+
+    def test_get_entity(self):
+        expected = 'some_other_sql_table'
+        self.query._entity = expected
+
+        assert self.query._get_entity() == expected
+
+    def test_set_columns(self):
+        columns = ['id', 'name', 'email']
+
+        self.query.set_columns(columns)
+
+        for idx, column in enumerate(columns):
+            assert self.query._columns[idx] == column
+
+    def test_set_row_0(self):
+        expected = ['1', "'my name'", "'email@somehost'"]
+
+        self.query.set_row([1, 'my name', 'email@somehost'])
+
+        for idx, row in enumerate(expected):
+            assert self.query._rows[0][idx] == row
+
+    def test_set_row_1(self):
+        expected_rows = [
+            ['1', "'my name'", "'email@somehost'"],
+            ['2', "'my other name'", "'other_email@somehost'"]
+        ]
+
+        self.query.set_row([1, 'my name', 'email@somehost'])
+        self.query.set_row([2, 'my other name', 'other_email@somehost'])
+
+        for row_idx, row in enumerate(self.query._rows):
+            for idx, expected_row in enumerate(expected_rows[row_idx]):
+                assert row[idx] == expected_row
+
+    def test_set_0(self):
+        expected_columns = ['id', 'name', 'email']
+        expected_row = ['1', "'my name'", "'email@somehost'"]
+
+        self.query.set({
+            'id': 1,
+            'name': 'my name',
+            'email': 'email@somehost'
+        })
+
+        for idx, column in enumerate(expected_columns):
+            assert self.query._columns[idx] == column
+
+        for idx, row in enumerate(expected_row):
+            assert self.query._rows[0][idx] == row
+
+    def test_set_1(self):
+        expected_columns = ['id', 'name', 'email']
+        expected_rows = [
+            ['1', "'my name'", "'email@somehost'"],
+            ['2', "'my other name'", "'other_email@somehost'"]
+        ]
+
+        self.query.set({
+            'id': 1,
+            'name': 'my name',
+            'email': 'email@somehost'
+        })
+
+        self.query.set({
+            'id': 2,
+            'name': 'my other name',
+            'email': 'other_email@somehost'
+        })
+
+        for idx, column in enumerate(expected_columns):
+            assert self.query._columns[idx] == column
+
+        for row_idx, row in enumerate(self.query._rows):
+            for idx, expected_row in enumerate(expected_rows[row_idx]):
+                assert row[idx] == expected_row
